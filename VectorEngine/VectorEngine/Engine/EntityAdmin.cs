976ca9664ffd3f97075bf08ca04f19eee1c01989
Allen Pestaluky <allenpestaluky@gmail.com>
2020-03-19 21:01:46 -0400

    Added a bunch more get tuple methods to the entity admin. More can be added if I ever need higher dimentioned tuples.

--- VectorEngine/VectorEngine/Engine/EntityAdmin.cs
+++ VectorEngine/VectorEngine/Engine/EntityAdmin.cs
@@ -13,11 +13,28 @@ namespace VectorEngine.Engine
         public List<ECSSystem> Systems = new List<ECSSystem>();
         public List<Component> Components = new List<Component>();
 
+        #region Tuples
+        public IEnumerable<T1> GetComponents<T1>() where T1 : Component
+        {
+            var result = new Dictionary<Entity, T1>();
+
+            foreach (var component in Components.Where( comp => comp is T1))
+            {
+                if (result.ContainsKey(component.Entity))
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                result[component.Entity] = (T1)component;
+            }
+
+            return result.Values.ToList();
+        }
+
         public IEnumerable<(T1, T2)> GetTuple<T1, T2>() where T1 : Component where T2 : Component
         {
             var tuples = new Dictionary<Entity, (T1, T2)>();
 
-            foreach (var component in Components.Where( comp => comp is T1))
+            foreach (var component in Components.Where(comp => comp is T1))
             {
                 if (!tuples.ContainsKey(component.Entity))
                 {
@@ -55,6 +72,144 @@ namespace VectorEngine.Engine
 
             return tuples.Values.ToList();
         }
+
+        public IEnumerable<(T1, T2, T3)> GetTuple<T1, T2, T3>() where T1 : Component where T2 : Component where T3 : Component
+        {
+            var tuples = new Dictionary<Entity, (T1, T2, T3)>();
+
+            foreach (var component in Components.Where(comp => comp is T1))
+            {
+                if (!tuples.ContainsKey(component.Entity))
+                {
+                    tuples[component.Entity] = (null, null, null);
+                }
+
+                var tuple = tuples[component.Entity];
+                if (tuples[component.Entity].Item1 != null)
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                tuple.Item1 = (T1)component;
+                tuples[component.Entity] = tuple;
+            }
+
+            foreach (var component in Components.Where(comp => comp is T2))
+            {
+                if (!tuples.ContainsKey(component.Entity))
+                {
+                    tuples[component.Entity] = (null, null, null);
+                }
+
+                var tuple = tuples[component.Entity];
+                if (tuples[component.Entity].Item2 != null)
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                tuple.Item2 = (T2)component;
+                tuples[component.Entity] = tuple;
+            }
+
+            foreach (var component in Components.Where(comp => comp is T3))
+            {
+                if (!tuples.ContainsKey(component.Entity))
+                {
+                    tuples[component.Entity] = (null, null, null);
+                }
+
+                var tuple = tuples[component.Entity];
+                if (tuples[component.Entity].Item3 != null)
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                tuple.Item3 = (T3)component;
+                tuples[component.Entity] = tuple;
+            }
+
+            // Remove the tuples that don't fit all the requriements:
+            tuples.RemoveAll((key, value) => value.Item1 == null);
+            tuples.RemoveAll((key, value) => value.Item2 == null);
+            tuples.RemoveAll((key, value) => value.Item3 == null);
+
+            return tuples.Values.ToList();
+        }
+
+        public IEnumerable<(T1, T2, T3, T4)> GetTuple<T1, T2, T3, T4>() where T1 : Component where T2 : Component where T3 : Component where T4 : Component
+        {
+            var tuples = new Dictionary<Entity, (T1, T2, T3, T4)>();
+
+            foreach (var component in Components.Where(comp => comp is T1))
+            {
+                if (!tuples.ContainsKey(component.Entity))
+                {
+                    tuples[component.Entity] = (null, null, null, null);
+                }
+
+                var tuple = tuples[component.Entity];
+                if (tuples[component.Entity].Item1 != null)
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                tuple.Item1 = (T1)component;
+                tuples[component.Entity] = tuple;
+            }
+
+            foreach (var component in Components.Where(comp => comp is T2))
+            {
+                if (!tuples.ContainsKey(component.Entity))
+                {
+                    tuples[component.Entity] = (null, null, null, null);
+                }
+
+                var tuple = tuples[component.Entity];
+                if (tuples[component.Entity].Item2 != null)
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                tuple.Item2 = (T2)component;
+                tuples[component.Entity] = tuple;
+            }
+
+            foreach (var component in Components.Where(comp => comp is T3))
+            {
+                if (!tuples.ContainsKey(component.Entity))
+                {
+                    tuples[component.Entity] = (null, null, null, null);
+                }
+
+                var tuple = tuples[component.Entity];
+                if (tuples[component.Entity].Item3 != null)
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                tuple.Item3 = (T3)component;
+                tuples[component.Entity] = tuple;
+            }
+
+            foreach (var component in Components.Where(comp => comp is T4))
+            {
+                if (!tuples.ContainsKey(component.Entity))
+                {
+                    tuples[component.Entity] = (null, null, null, null);
+                }
+
+                var tuple = tuples[component.Entity];
+                if (tuples[component.Entity].Item4 != null)
+                {
+                    throw new Exception("It appears as if an entity has two components of the same type!");
+                }
+                tuple.Item4 = (T4)component;
+                tuples[component.Entity] = tuple;
+            }
+
+            // Remove the tuples that don't fit all the requriements:
+            tuples.RemoveAll((key, value) => value.Item1 == null);
+            tuples.RemoveAll((key, value) => value.Item2 == null);
+            tuples.RemoveAll((key, value) => value.Item3 == null);
+            tuples.RemoveAll((key, value) => value.Item4 == null);
+
+            return tuples.Values.ToList();
+        }
+        #endregion
 
         #region Singleton Components (System States)
         public SingletonSampler SingletonSampler { get; } = new SingletonSampler();
