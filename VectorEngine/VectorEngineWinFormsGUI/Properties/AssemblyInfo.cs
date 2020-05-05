2850460129d24a5bda80c03961b0968a3c40682c
Allen Pestaluky <allenpestaluky@gmail.com>
2020-05-05 15:42:07 -0400

    Separated the WinFormsGUI from the engine library.

--- VectorEngine/VectorEngineWinFormsGUI/Properties/AssemblyInfo.cs
+++ VectorEngine/VectorEngineWinFormsGUI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VectorEngine")]
+[assembly: AssemblyTitle("Vector Engine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VectorEngine")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("Vector Engine")]
+[assembly: AssemblyCopyright("Copyright © Allen Pestaluky 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d91f5e44-7597-4f32-a9e4-7c930c711f17")]
+[assembly: Guid("beb6fffd-d207-47c4-81da-15d2d60294e2")]
 
 // Version information for an assembly consists of the following four values:
 //
