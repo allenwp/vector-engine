949297ef3e3520b66c60863c766e87b115d00c95
Allen Pestaluky <allenpestaluky@gmail.com>
2020-05-05 16:45:50 -0400

    WPF GUI now launches game engine with demo game scene. Other than the actual GUI part, this is now on par with the Win Forms GUI

--- VectorEngine/VectorEngineWPFGUI/App.xaml.cs
+++ VectorEngine/VectorEngineWPFGUI/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using VectorEngine;
 
 namespace VectorEngineWPFGUI
 {
@@ -13,5 +15,16 @@ namespace VectorEngineWPFGUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            GameLoop.SceneInit = VectorEngine.DemoGame.SceneEditorTest.Init;
+            Thread thread = new Thread(new ThreadStart(GameLoop.Loop));
+            thread.Name = "Game Loop Thread";
+            // this apartment state is required for the ASIOOutput.StartDriver method
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
     }
 }
