63e73d49d6dbfea9c7edf3769da4b5ebfcb0c479
Allen Pestaluky <allenpestaluky@gmail.com>
2020-05-09 22:14:54 -0400

    Started setting up the scene graph test

--- VectorEngine/VectorEngineDemoGame/DemoGame/SceneSceneGraphTest.cs
+++ VectorEngine/VectorEngineDemoGame/DemoGame/SceneSceneGraphTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,12 +14,29 @@ namespace VectorEngine.DemoGame
         {
             EntityAdmin.Instance.Systems.Add(new GamepadSystem());
             EntityAdmin.Instance.Systems.Add(new GamepadBasicFPSMovementSystem());
+            EntityAdmin.Instance.Systems.Add(new RotateSystem());
 
             // "Draw" systems:
             EntityAdmin.Instance.Systems.Add(new CameraSystem());
             EntityAdmin.Instance.Systems.Add(new SamplerSystem());
 
             EntityAdmin.Instance.CreateSingletons();
+
+            var entity = EntityAdmin.Instance.CreateEntity("Camera");
+            EntityAdmin.Instance.AddComponent<Camera>(entity);
+            EntityAdmin.Instance.AddComponent<Transform>(entity).LocalPosition = new Vector3(0,0,2f);
+            EntityAdmin.Instance.AddComponent<GamepadBasicFPSMovement>(entity);
+
+
+            entity = EntityAdmin.Instance.CreateEntity("Root GridPoint");
+            EntityAdmin.Instance.AddComponent<Rotate>(entity);
+            EntityAdmin.Instance.AddComponent<Transform>(entity);
+            EntityAdmin.Instance.AddComponent<GridPoint>(entity);
+
+            entity = EntityAdmin.Instance.CreateEntity("Root GridPoint");
+            EntityAdmin.Instance.AddComponent<Rotate>(entity);
+            EntityAdmin.Instance.AddComponent<Transform>(entity).LocalPosition = new Vector3(0.5f, 0, 0f);
+            EntityAdmin.Instance.AddComponent<GridPoint>(entity);
         }
     }
 }
