41f1193931632c952730549e0920ab699d57714e
Allen Pestaluky <allenpestaluky@gmail.com>
2020-05-11 17:53:02 -0400

    Added a console application that can host the game directly without a background thread.

---
+++ VectorEngine/VectorEngineConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VectorEngine;
+
+namespace VectorEngineConsole
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // this apartment state is required for the ASIOOutput.StartDriver method
+            Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
+            GameLoop.SceneInit = VectorEngine.DemoGame.SceneRotatingCubesAndGridPoints.Init;
+            GameLoop.Loop();
+        }
+    }
+}
