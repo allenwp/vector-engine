3c6eb44144fbf190a6641f355c1bc195e38522bf
Allen Pestaluky <allenpestaluky@gmail.com>
2020-05-26 16:06:45 -0400

    Added reference to Microsoft.Windows.SDK.Contracts NuGet to allow MIDI programming

---
+++ VectorEngine/VectorEngineWPFGUI/MIDI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Midi; // Needs Microsoft.Windows.SDK.Contracts NuGet Package as described here: https://docs.microsoft.com/en-us/windows/apps/desktop/modernize/desktop-to-uwp-enhance
+
+namespace VectorEngineWPFGUI
+{
+    public class MIDI
+    {
+        public static void GetDevices()
+        {
+            string midiInputQueryString = MidiInPort.GetDeviceSelector();
+        }
+    }
+}
