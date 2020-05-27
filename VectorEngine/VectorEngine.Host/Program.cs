9e75581779a7214c621f7ec6e7da8faf9a7edb88
Allen Pestaluky <allenpestaluky@gmail.com>
2020-05-27 16:27:42 -0400

    Created a new "Host" console 32 bit .Net Framework project that hosts the ImGui.NET editor and the game loop. I figure the editor can just be turned off and this can be the one project to rule them all, so I'm calling it the "Host" project.
    (In reference to my previous commit: I was wrong, I needed a new editor project 😂)

---
+++ VectorEngine/VectorEngine.Host/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using VectorEngine;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+using ImGuiNET;
+
+namespace VectorEngine.Host
+{
+    class Program
+    {
+        private static Sdl2Window _window;
+        private static GraphicsDevice _gd;
+        private static CommandList _cl;
+        private static ImGuiController _controller;
+
+        private static Vector3 _clearColor = new Vector3(0.45f, 0.55f, 0.6f);
+
+        static void Main(string[] args)
+        {
+            // Create window, GraphicsDevice, and all resources necessary for the demo.
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "Vector Engine Editor"),
+                new GraphicsDeviceOptions(true, null, true),
+                out _window,
+                out _gd);
+            _window.Resized += () =>
+            {
+                _gd.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+                _controller.WindowResized(_window.Width, _window.Height);
+            };
+            _cl = _gd.ResourceFactory.CreateCommandList();
+            _controller = new ImGuiController(_gd, _gd.MainSwapchain.Framebuffer.OutputDescription, _window.Width, _window.Height);
+
+            //GameLoop.Init(Flight.Scenes.Main.Init);
+
+            // Main application loop
+            while (_window.Exists)
+            {
+                //GameLoop.Tick();
+
+                InputSnapshot snapshot = _window.PumpEvents();
+                if (!_window.Exists) { break; }
+                _controller.Update(1f / 60f, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                SubmitUI();
+
+                _cl.Begin();
+                _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
+                _cl.ClearColorTarget(0, new RgbaFloat(_clearColor.X, _clearColor.Y, _clearColor.Z, 1f));
+                _controller.Render(_gd, _cl);
+                _cl.End();
+                _gd.SubmitCommands(_cl);
+                _gd.SwapBuffers(_gd.MainSwapchain);
+            }
+
+            // Clean up Veldrid resources
+            _gd.WaitForIdle();
+            _controller.Dispose();
+            _cl.Dispose();
+            _gd.Dispose();
+        }
+
+        private static unsafe void SubmitUI()
+        {
+            ImGui.Text("Hello, world!");
+        }
+    }
+}
