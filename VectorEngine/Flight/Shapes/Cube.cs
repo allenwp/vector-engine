312b66d58ecfc383b122205cc57143ec3bd747ec
Allen Pestaluky <allenpestaluky@gmail.com>
2020-08-26 13:17:27 -0400

    Fixed serialization for Cubes to not use sub-components, which messes with serialization.

--- VectorEngine/Flight/Shapes/Cube.cs
+++ VectorEngine/Flight/Shapes/Cube.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using VectorEngine;
@@ -10,7 +11,46 @@ namespace Flight.Shapes
 {
     public class Cube : Shape
     {
-        List<Line> lines;
+        /// <summary>
+        /// Copy of the Shapes.Line component, except not a Component because that causes a big mess
+        /// for serialization.
+        /// </summary>
+        private class Line
+        {
+            public float BaseFidelity;
+
+            public Vector3 Start;
+            public Vector3 End;
+
+            public Line()
+            {
+                BaseFidelity = 130;
+            }
+
+            public Line(float baseFidelity)
+            {
+                BaseFidelity = baseFidelity;
+            }
+
+            public List<Sample3D[]> GetSamples3D(float fidelity)
+            {
+                List<Sample3D[]> result = new List<Sample3D[]>(1);
+                int sampleLength = (int)Math.Round(BaseFidelity * fidelity);
+
+                Sample3D[] sample3DArray = new Sample3D[sampleLength];
+                for (int i = 0; i < sampleLength; i++)
+                {
+                    var point3D = Vector3.Lerp(Start, End, (float)i / (float)(sampleLength - 1));
+                    sample3DArray[i].Position = point3D;
+                    sample3DArray[i].Brightness = 1f;
+                }
+                result.Add(sample3DArray);
+
+                return result;
+            }
+        }
+
+        List<Line> cubeLines;
 
         public Cube() : this(50) { }
         public Cube(float baseFidelity)
@@ -20,28 +60,28 @@ namespace Flight.Shapes
 
         public void RecreateLines(float baseFidelity)
         {
-            lines = new List<Line>();
+            cubeLines = new List<Line>();
 
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, 0.5f, 0.5f), End = new Vector3(0.5f, 0.5f, 0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, 0.5f, 0.5f), End = new Vector3(0.5f, -0.5f, 0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, -0.5f, 0.5f), End = new Vector3(-0.5f, -0.5f, 0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, -0.5f, 0.5f), End = new Vector3(-0.5f, 0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, 0.5f, 0.5f), End = new Vector3(0.5f, 0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, 0.5f, 0.5f), End = new Vector3(0.5f, -0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, -0.5f, 0.5f), End = new Vector3(-0.5f, -0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, -0.5f, 0.5f), End = new Vector3(-0.5f, 0.5f, 0.5f) });
 
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, 0.5f, -0.5f), End = new Vector3(0.5f, 0.5f, -0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, 0.5f, -0.5f), End = new Vector3(0.5f, -0.5f, -0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, -0.5f, -0.5f), End = new Vector3(-0.5f, -0.5f, -0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, -0.5f, -0.5f), End = new Vector3(-0.5f, 0.5f, -0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, 0.5f, -0.5f), End = new Vector3(0.5f, 0.5f, -0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, 0.5f, -0.5f), End = new Vector3(0.5f, -0.5f, -0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, -0.5f, -0.5f), End = new Vector3(-0.5f, -0.5f, -0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, -0.5f, -0.5f), End = new Vector3(-0.5f, 0.5f, -0.5f) });
 
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, 0.5f, -0.5f), End = new Vector3(-0.5f, 0.5f, 0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, 0.5f, -0.5f), End = new Vector3(0.5f, 0.5f, 0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, -0.5f, -0.5f), End = new Vector3(0.5f, -0.5f, 0.5f) });
-            lines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, -0.5f, -0.5f), End = new Vector3(-0.5f, -0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, 0.5f, -0.5f), End = new Vector3(-0.5f, 0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, 0.5f, -0.5f), End = new Vector3(0.5f, 0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(0.5f, -0.5f, -0.5f), End = new Vector3(0.5f, -0.5f, 0.5f) });
+            cubeLines.Add(new Line(baseFidelity) { Start = new Vector3(-0.5f, -0.5f, -0.5f), End = new Vector3(-0.5f, -0.5f, 0.5f) });
         }
 
         public override List<Sample3D[]> GetSamples3D(float fidelity)
         {
             List<Sample3D[]> samples = new List<Sample3D[]>();
-            foreach (var line in lines)
+            foreach (var line in cubeLines)
             {
                 samples.AddRange(line.GetSamples3D(fidelity));
             }
