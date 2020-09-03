caa06e487e780a392f207036523eeab1faba4e75
Allen Pestaluky <allenpestaluky@gmail.com>
2020-09-03 17:48:28 -0400

    Determined the AspectRatio for the tektronix scopes that I have

--- VectorEngine/VectorEngine/Output/DisplayProfileOscTek2445.cs
+++ VectorEngine/VectorEngine/Output/DisplayProfileOscTek2445.cs
@@ -13,7 +13,7 @@ namespace VectorEngine.Output
     /// </summary>
     public class DisplayProfileOscTek2445 : DisplayProfile
     {
-        public override float AspectRatio { get; set; } = 4f / 3.5f;
+        public override float AspectRatio { get; set; } = 1.26f;
 
         public override float FullBrightnessOutput { get; set; } = -1f;
 
